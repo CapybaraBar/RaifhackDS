c8a4e3f752baa4093015e89e0f69646cb1ea7cc3
MrCheater <mister.cheater@list.ru>
2021-09-25 01:27:28 +0300

    WIP

--- predict.sh
+++ predict.sh
@@ -1,1 +1,1 @@
-poetry run python3 predict.py --model_path model.pickle --test_data data/test.csv --output data/test_submission2.csv
+poetry run python3 predict.py --model_path model.pickle --test_data data/test.csv --output solution.csv
